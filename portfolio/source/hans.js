dcaf70bacfe580eea88f377ed6f7da6b4abfa91b
Hans Chiu <chiuhans111@gmail.com>
2017-11-10 20:53:40 +0800

    portfolio online

--- portfolio/source/hans.js
+++ portfolio/source/hans.js
@@ -7,14 +7,23 @@ var data = {
     projects: [],
     info: {}
 }
+window.data = data;
+get('./projects.json').then(projects => {
+    data.projects = JSON.parse(projects).projects
+})
 
 get('./projects').then(projects => {
     data.projects = JSON.parse(projects).projects
 })
 
+get('./info.json').then(info => {
+    data.info = JSON.parse(info).user
+})
+
 get('./hansinfo').then(info => {
     data.info = JSON.parse(info).user
 })
+
 
 common.Vue.component('test', test);
 
