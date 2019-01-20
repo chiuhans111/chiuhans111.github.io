b5d7fa224cb9a3188635fd687eb7f8152bee9717
Hans Chiu <chiuhans111@gmail.com>
2019-01-20 18:05:09 +0800

    organize

--- UML/src/Block.ts
+++
@@ -1,8 +0,0 @@
-class Block implements ISize {
-    width: Number;
-    height: Number;
-    constructor(w: Number, h: number) {
-        this.width = w;
-        this.height = h;
-    }
-}
--- UML/src/main.ts
+++
@@ -1,3 +0,0 @@
-var a = new Block(10, 10)
-var b = new Block(20, 40)
-Layout.arrange([a, b])
